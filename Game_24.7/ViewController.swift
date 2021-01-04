a516e0e18178d8065a473ffa6685c5710a1d07f8
fujified <uktras@gmail.com>
2021-01-04 22:31:09 +0300

    Initial Commit

---
+++ Game_24.7/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Game_24.7
+//
+//  Created by mr spock on 1/4/21.
+//  Copyright © 2021 mr spock. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

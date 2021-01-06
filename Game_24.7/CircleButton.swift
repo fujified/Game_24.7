ad3bfefe07c6c8dce9186df3be47a3c6c4ae897a
fujified <uktras@gmail.com>
2021-01-06 15:20:51 +0300

    update

---
+++ Game_24.7/CircleButton.swift
@@ -0,0 +1,29 @@
+//
+//  CircleButton.swift
+//  Game_24.7
+//
+//  Created by mr spock on 1/6/21.
+//  Copyright © 2021 mr spock. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class CircleButton: UIButton {
+
+    
+    @IBInspectable var roundButton: Bool = false {
+        didSet {
+            if roundButton {
+                layer.cornerRadius = frame.height / 2
+            }
+        }
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        if roundButton {
+            layer.cornerRadius = frame.height / 2
+        }
+    }
+    
+}
